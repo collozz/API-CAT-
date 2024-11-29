eb3e4a2825e562b3d4b924d02bb73e86b61193e3
collozz <cyrus.gitonga@strathmore.edu>
2024-11-29 16:45:31 +0300

    modified:   APICAT/APICAT2/models.py

--- APICAT/APICAT2/models.py
+++ APICAT/APICAT2/models.py
@@ -8,3 +8,4 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
     order_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
